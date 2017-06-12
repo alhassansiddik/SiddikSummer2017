80e9813fdd7263dfc3d3b5268c60f3b54dc54e94
alhassansiddik <alhassansiddik@yahoo.com>
2017-06-12 12:52:51 -0400

    Initial commit

---
+++ app/src/main/java/com/siddik/siddiksummer2017/NewActivity.java
@@ -0,0 +1,17 @@
+package com.siddik.siddiksummer2017;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sidleh on 6/12/2017.
+ */
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
